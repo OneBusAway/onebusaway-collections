8f12d5d5db3c1f22ba5202f3c955eb789997fc98
Brian Ferris <bdferris@google.com>
2011-05-04 22:56:37 +0000

    Add constructors to Range class

--- src/main/java/org/onebusaway/collections/Range.java
+++ src/main/java/org/onebusaway/collections/Range.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011 Brian Ferris <bdferris@onebusaway.org>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.onebusaway.collections;
 
@@ -26,6 +26,19 @@ public class Range {
   private double _min = Double.POSITIVE_INFINITY;
 
   private double _max = Double.NEGATIVE_INFINITY;
+
+  public Range() {
+
+  }
+
+  public Range(double v) {
+    addValue(v);
+  }
+
+  public Range(double from, double to) {
+    addValue(from);
+    addValue(to);
+  }
 
   public void addValue(double value) {
     _min = Math.min(_min, value);
