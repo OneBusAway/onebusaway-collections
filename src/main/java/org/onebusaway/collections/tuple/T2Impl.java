1457c67a4b1c409316ebaa6f8ef37b5f85ece0d8
Brian Ferris <bdferris@google.com>
2012-04-15 10:28:33 +0200

    Add a toString() method to the T2 tuple implementation class.

--- src/main/java/org/onebusaway/collections/tuple/T2Impl.java
+++ src/main/java/org/onebusaway/collections/tuple/T2Impl.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2011 Brian Ferris <bdferris@onebusaway.org>
+ * Copyright (C) 2012 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,5 +68,10 @@ final class T2Impl<S1, S2> implements T2<S1, S2>, Serializable {
     T2Impl<?, ?> other = (T2Impl<?, ?>) obj;
     return Tuples.equals(_first, other._first)
         && Tuples.equals(_second, other._second);
+  }
+  
+  @Override
+  public String toString() {
+    return _first + ", " + _second;
   }
 }
