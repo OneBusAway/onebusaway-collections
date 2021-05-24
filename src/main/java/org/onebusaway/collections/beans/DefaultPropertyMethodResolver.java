29549e60592227afe3d213057862d4aab1c90059
Jeff Maki <jmaki@camsys.com>
2021-05-24 18:09:25 -0400

    Fix for interface-backed stop entities

--- src/main/java/org/onebusaway/collections/beans/DefaultPropertyMethodResolver.java
+++ src/main/java/org/onebusaway/collections/beans/DefaultPropertyMethodResolver.java
@@ -16,17 +16,49 @@
 package org.onebusaway.collections.beans;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
 
 public class DefaultPropertyMethodResolver implements PropertyMethodResolver {
+
+  private ClassGraph classScanner = new ClassGraph();
 
   @Override
   public PropertyMethod getPropertyMethod(Class<?> targetType,
       String propertyName) {
-    String methodName = "get" + propertyName.substring(0, 1).toUpperCase()
-        + propertyName.substring(1);
+	String methodName = "get";
+	for(String part : propertyName.split(" |_")) {
+	    methodName += part.substring(0, 1).toUpperCase()
+	            + part.substring(1);
+	}
     Method method = null;
     try {
-      method = targetType.getMethod(methodName);
+    	if(targetType.isInterface()) {
+        	ScanResult scanResult = new ClassGraph()
+        			.acceptPackages("org.onebusaway.gtfs")
+        			.enableClassInfo()
+        			.scan();
+
+        	List<Method> methods = new ArrayList<Method>();
+        	for (ClassInfo ci : scanResult.getClassesImplementing(targetType.getCanonicalName())) {
+        		try {
+            		methods.add(Class.forName(ci.getName()).getMethod(methodName));
+        		} catch(Exception e) {
+        			continue;
+        		}
+        	}	
+        	
+        	if(methods.size() == 1)
+        		method = methods.get(0);
+        	else
+        	    throw new IllegalStateException("Ambiguous implementation set for interface: "
+        	              + targetType + " potentials: " + methods);
+    	} else
+        	method = targetType.getMethod(methodName);
     } catch (Exception ex) {
       throw new IllegalStateException("error introspecting class: "
           + targetType, ex);
